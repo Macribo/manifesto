facf1bdb77741227b918bd7bbed8e5f3fd1d2dfe
macribo <roibeard.mac.unfraidh@gmail.com>
2017-07-08 19:10:46 +0200

    move narrative.js into manifesto.js. update Makefile accordingly

--- characterIsDestiny/src/bundle.js
+++ characterIsDestiny/src/bundle.js
@@ -2,8 +2,6 @@
 //jshint esversion:6
 
 let storyTexts = require("./story-texts"); //can't use capital letters with browswerify 
-let narrative = require("./narrative");
-narrative(storyTexts);
 
 //Create the map
 
@@ -91,6 +89,7 @@ var image = document.querySelector("img");
 
 //The input and output fields
 var output = document.querySelector("#output");
+var output2 = document.querySelector("#output2");
 var input = document.querySelector("#input");
 
 //The button
@@ -135,111 +134,22 @@ function keydownHandler(event)
   }
 }
 
+
+let story = 0;
+function narrate(story){
+   output2.innerHTML = storyTexts[story];
+console.log("test! "+story);
+}
+//test
+
+
+
 function playGame()
 {
-  //Get the player's input and convert it to lowercase
-  playersInput = input.value;
-  playersInput = playersInput.toLowerCase();
-  
-  //Reset these variables from the previous turn
-  gameMessage = "";
-  action = "";
-  
-  //Figure out the player's action
-  for(i = 0; i < actionsIKnow.length; i++)
-  {
-    if(playersInput.indexOf(actionsIKnow[i]) !== -1)
-    {
-      action = actionsIKnow[i];
-      console.log("player's action: " + action);
-      break;
-    }
-  }
-  
-  //Figure out the item the player wants
-  for(i = 0; i < itemsIKnow.length; i++)
-  {
-    if(playersInput.indexOf(itemsIKnow[i]) !== -1)
-    {
-      item = itemsIKnow[i];
-      console.log("player's item: " + item);
-    }
-  }
-  
-  //Choose the correct action
-  switch(action)
-  {
-    case "north":
-      if(mapLocation >= 3)
-      {
-        mapLocation -= 3;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-    
-    case "east":
-	    if(mapLocation % 3 != 2)
-      {
-        mapLocation += 1;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "south":
-      if(mapLocation < 6)
-      {
-        mapLocation += 3;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "west":
-      if(mapLocation % 3 !== 0)
-      {
-        mapLocation -= 1;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "help":
-      //Display a hint if there is one for this location
-      if(helpMessages[mapLocation] !== "")
-      {
-        gameMessage = helpMessages[mapLocation] + " ";
-      }
-      gameMessage += "Try any of these words: "; 
-      gameMessage += "north, east, south, west, take, drop, ";
-      gameMessage += "use, stone, flute, sword.";
-      break;
-      
-    case "take":
-      takeItem();
-		  break;
-		
-		case "drop":
-		  dropItem();
-		  break;
-		  
-		case "use":
-		  useItem();
-		  break;
-    		  
-		default:
-		  gameMessage = "I don't understand that.";
-  }
-  
+ 
+ narrate(story); 
+    console.log(story);
+    story++;
   //Render the game
   render();
 }
@@ -423,30 +333,18 @@ function render()
   
   //Display the game message
 
-    output.style.fontSize="xx-large";
+    output.style.fontSize="42px";
+    output.style.textAlign="center";
     output.innerHTML += gameMessage ;
-  
+    output.style.padding=0;
+    output.style.margin=0;
   //Clear the input field
   input.value = "";
 }
 
 
 
-},{"./narrative":2,"./story-texts":3}],2:[function(require,module,exports){
-//jshint esversion:6
-module.exports = function narrative(storyTexts) {
-    let storyText = document.querySelector("#output2");
-    storyText.innerHTML = storyTexts[0];
-    let story = 0;
-    storyText.addEventListener("click", function(event){
-        story++;
-        storyText.innerHTML = storyTexts[story];
-        //console.log(story);
-    });
-
-};
-
-},{}],3:[function(require,module,exports){
+},{"./story-texts":2}],2:[function(require,module,exports){
 //jshint esversion:6
 module.exports = [
 
--- characterIsDestiny/src/manifesto.js
+++ characterIsDestiny/src/manifesto.js
@@ -1,8 +1,6 @@
 //jshint esversion:6
 
 let storyTexts = require("./story-texts"); //can't use capital letters with browswerify 
-let narrative = require("./narrative");
-narrative(storyTexts);
 
 //Create the map
 
@@ -90,6 +88,7 @@ var image = document.querySelector("img");
 
 //The input and output fields
 var output = document.querySelector("#output");
+var output2 = document.querySelector("#output2");
 var input = document.querySelector("#input");
 
 //The button
@@ -134,111 +133,22 @@ function keydownHandler(event)
   }
 }
 
+
+let story = 0;
+function narrate(story){
+   output2.innerHTML = storyTexts[story];
+console.log("test! "+story);
+}
+//test
+
+
+
 function playGame()
 {
-  //Get the player's input and convert it to lowercase
-  playersInput = input.value;
-  playersInput = playersInput.toLowerCase();
-  
-  //Reset these variables from the previous turn
-  gameMessage = "";
-  action = "";
-  
-  //Figure out the player's action
-  for(i = 0; i < actionsIKnow.length; i++)
-  {
-    if(playersInput.indexOf(actionsIKnow[i]) !== -1)
-    {
-      action = actionsIKnow[i];
-      console.log("player's action: " + action);
-      break;
-    }
-  }
-  
-  //Figure out the item the player wants
-  for(i = 0; i < itemsIKnow.length; i++)
-  {
-    if(playersInput.indexOf(itemsIKnow[i]) !== -1)
-    {
-      item = itemsIKnow[i];
-      console.log("player's item: " + item);
-    }
-  }
-  
-  //Choose the correct action
-  switch(action)
-  {
-    case "north":
-      if(mapLocation >= 3)
-      {
-        mapLocation -= 3;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-    
-    case "east":
-	    if(mapLocation % 3 != 2)
-      {
-        mapLocation += 1;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "south":
-      if(mapLocation < 6)
-      {
-        mapLocation += 3;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "west":
-      if(mapLocation % 3 !== 0)
-      {
-        mapLocation -= 1;
-      }
-      else
-      {
-        gameMessage = blockedPathMessages[mapLocation];
-      }
-      break;
-      
-    case "help":
-      //Display a hint if there is one for this location
-      if(helpMessages[mapLocation] !== "")
-      {
-        gameMessage = helpMessages[mapLocation] + " ";
-      }
-      gameMessage += "Try any of these words: "; 
-      gameMessage += "north, east, south, west, take, drop, ";
-      gameMessage += "use, stone, flute, sword.";
-      break;
-      
-    case "take":
-      takeItem();
-		  break;
-		
-		case "drop":
-		  dropItem();
-		  break;
-		  
-		case "use":
-		  useItem();
-		  break;
-    		  
-		default:
-		  gameMessage = "I don't understand that.";
-  }
-  
+ 
+ narrate(story); 
+    console.log(story);
+    story++;
   //Render the game
   render();
 }
@@ -422,9 +332,11 @@ function render()
   
   //Display the game message
 
-    output.style.fontSize="xx-large";
+    output.style.fontSize="42px";
+    output.style.textAlign="center";
     output.innerHTML += gameMessage ;
-  
+    output.style.padding=0;
+    output.style.margin=0;
   //Clear the input field
   input.value = "";
 }
