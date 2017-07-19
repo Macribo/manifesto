a738c4eeb247dd34fa85a6c9a34ace650deb3ca0
macribo <roibeard.mac.unfraidh@gmail.com>
2017-07-19 02:03:57 +0200

    fix translation text spacing. Edit manifesto.

--- manifesto/src/bundle.js
+++ manifesto/src/bundle.js
@@ -665,7 +665,6 @@ module.exports = [
 `<span id='Welcome'>Fáilte.</span>
 
 
-<span id='Select'>Roghnaigh</span> <span id='a team'>foireann</span>
 `,
   
 
@@ -690,13 +689,13 @@ module.exports = [
 //4
 `
 
-<span id='In English'>I mBearla</span>, <span id='it is that'>tá</span> <span id='the Irish'>an Gaeilge</span> <span id='utterly separated'>glanscartha</span> <span id='from'>ó</span> <span id='the meaning'>brí</span> <span id='a'>an Gaeilge</span>.
+<span id='In English'>I mBearla</span>, <span id='it is that'>tá</span> <span id='the Irish'>an Gaeilge</span> <span id='utterly separated'>glanscartha</span> <span id='from'>ó</span> <span id='the meaning'>brí</span> <span id='of Irish'>an Gaeilge</span>.
 
 `,
 
 //5
 `
-<span id='Leaves'>Fágann</span> <span id='English'>Bearla</span> <span id='us'>muid</span> <span id='discussing'>ag plé</span> <span id='the question'>ceist</span> <span id='of the tongue'>an teanga</span> <span id='as though'>amhlaidh is</span> <span id='that'>gur</span> <span id='an ornament'>ornáid</span> <span id='decorative'>maisaithe</span> <span id='cultural'>cultúrtha</span> <span id='the Irish language is'>í an Gaeilge</span>.
+<span id='English leaves'>Fágann Bearla</span> <span id='us'>muid</span> <span id='discussing'>ag plé</span> <span id='the question'>ceist</span> <span id='of the tongue'>an teanga</span> <span id='as though'>amhlaidh is</span> <span id='that'>gur</span> <span id='an ornament'>ornáid</span> <span id='decorative'>maisaithe</span> <span id='cultural'>cultúrtha</span> <span id='the Irish language is'>í an Gaeilge</span>.
 
 
 `,
@@ -704,14 +703,14 @@ module.exports = [
 //6
 
 `
-<span id='We are left'>Fágtar muid</span> <span id='speaking'>ag caint</span> <span id='in terms'>i dtearmaí</span> <span id='of the number of'>líonn</span> 'Irish Speakers'. <span id='It is seen'>Feictear</span> <span id='that there are not'>nach bhfuil</span> <span id='many'>múrán</span><span id='speaking it'> á labhairt</span>, <span id='and'>agus</span><span id='it is assumed'> glachtar leis</span> <span id='therefore'>mar sin <span id='not much would be lost'>nach chailfar murán</span> <span id='without her'>gan í.</span> 
+<span id='We are left'>Fágtar muid</span> <span id='speaking'>ag caint</span> <span id='in terms'>i dtearmaí</span> <span id='of the number of'>líonn</span><span id='Irish Speakers'> 'Irish Speakers'</span>. <span id='It is observed'>Feictear</span> <span id='that there are not'>nach bhfuil</span> <span id='many'>mórán</span><span id='speakers'> cainteoirí</span>, <span id='and'>agus </span><span id='it is assumed'>glachtar leis</span> <span id='therefore'>mar sin</span> <span id='not much would be lost'>nach chailfar mórán</span> <span id='without her'>gan í.</span> 
 
 
 `,
 
 //7
 `
-<span id='Because'>Toisc</span> <span id='it is possible'>gur feidir <span id='any'>aon</span> <span id='notion'>nóisean</span> <span id='to translate to English'>a aistrú go Bearla</span>,<span id="there isn't any need"> níl aon gá</span><span id='for Irish'> le Gaeilge</span>.<span id='In English'> I mBearla</span>,<span id='there is no need for'> níl aon gá le</span><span id='Éire'> Éire</span>, <span id='because'>mar</span> <span id='is enough'>is leoir</span> <span id='Ireland'>'Ireland'</span>.
+<span id='It is thought'>Ceaptar, </span><span id='perhaps'>b'feidir</span>, <span id='if all can'>más feidir gach rud</span> <span id='to translate to English'>a aistrú go Bearla</span>,<span id="there is no need"> níl gá</span><span id='for Irish'> le Gaeilge</span>.<span id='In English'> I mBearla</span>,<span id='there is no need for'> níl aon gá le</span><span id='Éire'> Éire</span>, <span id='because'>mar</span> <span id='is enough'>is leoir</span> <span id='Ireland'>'Ireland'</span>.
 
 
 
@@ -723,13 +722,13 @@ module.exports = [
 
 `
 <span id='But'>Ach</span> <span id='there is'>tá</span> <span id='more'>nios mó</span> <span id='in question'>i gceist</span> <span id='with'>le</span> <span id='language'>teanga</span> <span id='than'>ná</span><span id='talk'> caint</span>. <span id='A system'>Comhras</span> <span id='of thought'>smaoineamh</span> <span id='is Gaelic'>í an Ghaeilge</span>. <span id='An ecology'>Eiceachóras</span> <span id='of visions'>físanna</span> <span id='the'>na</span> <span id='thousands'>mílte</span><span id='of years'> blianta</span><span id='of age'> d'aois</span><span id='she is'> í</span>. <span id='That is'>Sin</span> <span id='a powerful thing'>rud laidir</span>. 
-<span id='Here'>Seo</span><span id='another powerful thing'> rud laidir eile</span>:
+<span id='Here is'>Seo</span><span id='powerful thing'> rud laidir</span><span id='another'> eile</span>:
 
 `,
 
 //9
  `   
-<span id='there is'>tá</span> <span id='a bit of Irish'>Gaeilge éigin</span> <span id='posessed by'>ag</span> <span id='every'>gach</span> <span id='English-speaker'>Bearl-cainteoir</span> <span id='in the country'>sa tír</span>.<span='It is not'> Ní</span> <span id='a subject'>ábhar <span id='of mockery'>fachnaoid</span> <span id='those'>iad</span> <span id='the'>na</span><span id='so-called few words'> cúpla focal. <span id='Seeds'>Síolta</span><span id='they are'> íad</span>. 
+<span id='There is'>Tá</span> <span id='a bit of Irish'>Gaeilge éigin</span> <span id='posessed by'>ag</span> <span id='every'>gach</span> <span id='English-speaker'>Bearl-cainteoir</span> <span id='in the country'>sa tír</span>.<span id='It is not'> Ní</span> <span id='a subject'>ábhar </span><span id='of mockery'>fachnaoid</span> <span id='those'>iad</span> <span id='the'>na</span><span id='so-called few words'> cúpla focal</span>. <span id='They are seeds'>Síolta íad</span>. 
 
 
 `,
@@ -737,32 +736,33 @@ module.exports = [
 //10
 `
 
-<span id='It is not'>Ní </span><span id='an absence'>easpa</span> <span id='of Irish'>Gaeilge</span> <span id='which'>is</span> <span id='is the cause'>cúis</span><span id='of our'> len ár</span> <span id='riddle'>dúcheist</span> <span id='language'>teanga</span>. <span id='Nor'>Ná</span> <span id='absence'>easpa</span><span id='interest'> spéis</span>, <span id='nor ability'>ná cumas</span>, <span id='nor'>ná</span> <span id='will'>thoill</span><span id='either'> ach oiread</span>.
+<span id='It is not'>Ní </span><span id='an absence'>easpa</span> <span id='of Irish'>Gaeilge</span> <span id='which'>is</span> <span id='is the cause'>cúis</span><span id='of our'> len ár</span> <span id='language question'>dúcheist teanga</span>. <span id='Nor'>Ná</span> <span id='an absence'>easpa</span> <span id='of interest'> spéis</span>, <span id='nor ability'>ná cumas</span>, <span id='nor will'>ná thoill</span><span id='either'> ach oiread</span>.
 
 `,
 
 //11
 //12
 `
-<span id='An absence'>Easpa</span> <span id='of room'>spás</span> <span id='is the cause'>is cúis</span><span id='of/with it'> leis</span>. <span id='There is no'>Níl</span> <span id='room'>spás</span> <span id='in'>sa</span> <span id='the imagination'>samhlíocht</span> <span id='English'>Bearla</span> <span id='for'>i gcomhair</span> <span id='thoughts'>smaointe</span><span id='Gaelic'> Gaelach</span>. 
+<span id='An absence'>Easpa</span> <span id='of room'>spás</span> <span id='is the cause'>is cúis</span><span id='of it'> leis</span>. <span id='There is no'>Níl</span> <span id='room'>spás</span> <span id='in'>sa</span> <span id='English thought'>samhlíocht Bearla</span> <span id='for'>i gcomhair</span> <span id='thoughts'>smaointe</span><span id='Gaelic'> Gaelach</span>. 
 
 `,
 
 
 //13
 `
-<span id='The aim'>Cuspoir</span> <span id='of this manifesto'>an forógra seo</span> <span id='is that'>ná</span> <span id='it would help'>go cuideoidh <span id='a virtual space'>spás fíorúil</span> <span id='with us'>linn</span> <span id='to exist as'>bheith</span><span id='Gaels'> Gaelach</span>. 
+<span id='The aim'>Cuspoir</span> <span id='of this manifesto'>an forógra seo</span> <span id='is that'>ná</span> <span id='it would help'>go cuideoidh</span> <span id='a virtual space'>spás fíorúil</span> <span id='with us'>linn</span> <span id='to exist as'>bheith</span><span id='Gaels'> Gaelach</span>. 
 
 `,
 
 //14
-`<span id='Imagine'>Samhlaigh</span> <span id='a platform'>árdán</span> <span id='social'>socialta</span> <span id='multifunctional'>ilfeidhmnú</span> <span id='in which one could'>inar feidir</span>: 
-
-<span id='Ourselves'>Muid fein <span id='identify'>a sainú</span> <span id='as'>mar</span> Gael, <span id='in a'>i</span> <span id='"land"'>"tír"</span> <span id='Gaelic'>Gaelach</span> <span id='digital'>digitach</span>
-<span id='Resources'>Áisanna</span> <span id='Gaelic'>Gaelach</span> <span id='discover'>a</span> aimsú <span id='and'>agus</span> <span id='share'>a roinnt</share>.
+`<span id='Imagine'>Samhlaigh</span> <span id='a platform'>árdán</span> <span id='social'>socialta</span> <span id='virtual'>fíorúil</span> <span id='in which one could'>inar feidir</span>: 
+<br>
+    <ul><li>
+<span id='Ourselves'>Muid fein</span> <span id='identify'>a sainú</span> <span id='as'>mar</span> Gael, <span id='in a'>i</span> <span id='"land"'>"tír"</span> <span id='Gaelic'>Gaelach</span> <span id='digital'>digitach</span></li><li>
+<span id='Resources'>Áisanna</span> <span id='Gaelic'>Gaelach</span> <span id='discover'>a aimsú</span> <span id='and'>agus</span> <span id='share'>a roinnt</share>.</li><li>
 <span id='Co-operate'>Comhoibrú</span> <span id='on'>ar</span><span id='projects'> tionscanamh</span> <span id='and'>agus</span><span id='business ventures'> gnó</span>.
-
-
+</li>
+</ul>
 `,
 
 //15
@@ -774,6 +774,7 @@ module.exports = [
 
 //16
 `
+<span id='Select'>Roghnaigh</span> <span id='your team'>foireann</span>
 `, 
   
 //17
